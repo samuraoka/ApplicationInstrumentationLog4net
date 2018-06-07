56fa4f46bc5115491638f9327b39c3d2c16dd307
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-06-07 21:26:48 +0900

    Implemented some code to read configuration from App.config file.

--- Configuration/3_AssemblyAttribute/Program.cs
+++ Configuration/3_AssemblyAttribute/Program.cs
@@ -1,15 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using log4net;
+using log4net.Config;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace _3_AssemblyAttribute
+// For this setting, XmlConfigurator read configuration from App.config file. 
+[assembly: XmlConfigurator]
+
+namespace AssemblyAttribute
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // log4net
+            // https://www.nuget.org/packages/log4net/
+            // Install-Package -Id log4net -ProjectName 3_AssemblyAttribute
+            var logger = LogManager.GetLogger(typeof(Program));
+
+            // output loggin
+            Task.Run(() => logger.Debug("Debug"));
+            Thread.Sleep(1000);
+
+            Task.Run(() => logger.Info("Info"));
+            Thread.Sleep(1000);
+
+            Task.Run(() => logger.Warn("Warn"));
+            Thread.Sleep(1000);
+
+            Task.Run(() => logger.Error("Error"));
+            Thread.Sleep(1000);
+
+            Task.Run(() => logger.Fatal("Fatal"));
+            Thread.Sleep(1000);
         }
     }
 }
