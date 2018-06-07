f544e4699ea5c37d00e1c113bc31dd3e78c2d143
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-06-07 18:38:22 +0900

    Implemented some code to demonstrate how to log by log4net.

--- Demo/Demo/Program.cs
+++ Demo/Demo/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using log4net;
+using log4net.Config;
+using System.Reflection;
 
 namespace Demo
 {
@@ -6,7 +8,16 @@ namespace Demo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // log4net
+            // https://www.nuget.org/packages/log4net/
+            // Install-Package -Id log4net -ProjectName Demo
+            //
+            // https://stackify.com/making-log4net-net-core-work/
+            // http://crbtech.in/Dot-Net-Training/ways-to-configure-log4net-for-dotnet-core/
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            BasicConfigurator.Configure(logRepository);
+            var log = LogManager.GetLogger(typeof(Program));
+            log.Info("Hello from log4net!");
         }
     }
 }
