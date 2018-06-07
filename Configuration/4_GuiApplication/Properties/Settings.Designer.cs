7855815659345caca403718e031914c409ceaf43
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-06-07 21:38:41 +0900

    Changed some namespace

--- Configuration/4_GuiApplication/Properties/Settings.Designer.cs
+++ Configuration/4_GuiApplication/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace _4_GuiApplication.Properties
+namespace GuiApplication.Properties
 {
 
 
