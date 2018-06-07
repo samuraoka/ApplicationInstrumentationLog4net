b41103cc869eb0e23bb44e267f07dba207a24920
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-06-07 20:54:30 +0900

    Added some configuration files for log4net.

--- Configuration/2_XmlConfiguration/Program.cs
+++ Configuration/2_XmlConfiguration/Program.cs
@@ -1,12 +1,47 @@
-﻿using System;
+﻿using log4net;
+using log4net.Config;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace XmlConfiguration
 {
     class Program
     {
+        private const string ConfigFilenameDefault = "log4net.config";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var configFIlename = ConfigFilenameDefault;
+            if (args.Length != 0)
+            {
+                // Check if a string is a valid Windows directory (folder) path
+                // https://stackoverflow.com/questions/3137097/check-if-a-string-is-a-valid-windows-directory-folder-path
+                configFIlename = Path.GetFullPath(args[0]);
+            }
+
+            // log4net
+            // https://www.nuget.org/packages/log4net/
+            // Install-Package -Id log4net -ProjectName 2_XmlConfiguration
+            var repo = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(repo, new FileInfo(configFIlename));
+            var logger = LogManager.GetLogger(typeof(Program));
+
+            Task.Run(() => logger.Debug("Debug"));
+            Thread.Sleep(1000);
+
+            Task.Run(() => logger.Info("Info"));
+            Thread.Sleep(1000);
+
+            Task.Run(() => logger.Warn("Warn"));
+            Thread.Sleep(1000);
+
+            Task.Run(() => logger.Error("Error"));
+            Thread.Sleep(1000);
+
+            Task.Run(() => logger.Fatal("Fatal"));
+            Thread.Sleep(1000);
         }
     }
 }
